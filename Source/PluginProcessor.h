0daacbc0de24edccf874646365e03f7f29ffd8b0
Anthony <anthony.passero121@gmail.com>
2018-10-08 22:58:16 -0400

    initial commit

---
+++ Source/PluginProcessor.h
@@ -0,0 +1,163 @@
+/*
+  ==============================================================================
+
+    This file was auto-generated!
+
+    It contains the basic framework code for a JUCE plugin processor.
+
+  ==============================================================================
+*/
+
+#pragma once
+
+#include "../JuceLibraryCode/JuceHeader.h"
+
+//==============================================================================
+/**
+*/
+class SuperCoolSynthAudioProcessor  : public AudioProcessor
+{
+public:
+    //==============================================================================
+    SuperCoolSynthAudioProcessor();
+    ~SuperCoolSynthAudioProcessor();
+
+    //==============================================================================
+    void prepareToPlay (double sampleRate, int samplesPerBlock) override;
+    void releaseResources() override;
+	void setUsingSineWaveSound();
+
+   #ifndef JucePlugin_PreferredChannelConfigurations
+    bool isBusesLayoutSupported (const BusesLayout& layouts) const override;
+   #endif
+
+    void processBlock (AudioBuffer<float>&, MidiBuffer&) override;
+
+    //==============================================================================
+    AudioProcessorEditor* createEditor() override;
+    bool hasEditor() const override;
+
+    //==============================================================================
+    const String getName() const override;
+
+    bool acceptsMidi() const override;
+    bool producesMidi() const override;
+    bool isMidiEffect() const override;
+    double getTailLengthSeconds() const override;
+
+    //==============================================================================
+    int getNumPrograms() override;
+    int getCurrentProgram() override;
+    void setCurrentProgram (int index) override;
+    const String getProgramName (int index) override;
+    void changeProgramName (int index, const String& newName) override;
+
+    //==============================================================================
+    void getStateInformation (MemoryBlock& destData) override;
+    void setStateInformation (const void* data, int sizeInBytes) override;
+
+private:
+    //==============================================================================
+
+	Synthesiser synth;
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (SuperCoolSynthAudioProcessor)
+};
+
+
+//BELOW IS FROM THE TUTORIAL
+
+//==============================================================================
+struct SineWaveSound : public SynthesiserSound
+{
+	SineWaveSound() {}
+
+	bool appliesToNote(int) override { return true; }
+	bool appliesToChannel(int) override { return true; }
+};
+
+//==============================================================================
+struct SineWaveVoice : public SynthesiserVoice
+{
+	SineWaveVoice() {}
+
+	bool canPlaySound(SynthesiserSound* sound) override
+	{
+		return dynamic_cast<SineWaveSound*> (sound) != nullptr;
+	}
+
+	void startNote(int midiNoteNumber, float velocity,
+		SynthesiserSound*, int /*currentPitchWheelPosition*/) override
+	{
+		currentAngle = 0.0;
+		level = velocity * 0.15;
+		tailOff = 0.0;
+
+		auto cyclesPerSecond = MidiMessage::getMidiNoteInHertz(midiNoteNumber);
+		auto cyclesPerSample = cyclesPerSecond / getSampleRate();
+
+		angleDelta = cyclesPerSample * 2.0 * MathConstants<double>::pi;
+	}
+
+	void stopNote(float /*velocity*/, bool allowTailOff) override
+	{
+		if (allowTailOff)
+		{
+			if (tailOff == 0.0)
+				tailOff = 1.0;
+		}
+		else
+		{
+			clearCurrentNote();
+			angleDelta = 0.0;
+		}
+	}
+
+	void pitchWheelMoved(int) override {}
+	void controllerMoved(int, int) override {}
+
+	void renderNextBlock(AudioSampleBuffer& outputBuffer, int startSample, int numSamples) override
+	{
+		if (angleDelta != 0.0)
+		{
+			if (tailOff > 0.0) // [7]
+			{
+				while (--numSamples >= 0)
+				{
+					auto currentSample = (float)(std::sin(currentAngle) * level * tailOff);
+
+					for (auto i = outputBuffer.getNumChannels(); --i >= 0;)
+						outputBuffer.addSample(i, startSample, currentSample);
+
+					currentAngle += angleDelta;
+					++startSample;
+
+					tailOff *= 0.99; // [8]
+
+					if (tailOff <= 0.005)
+					{
+						clearCurrentNote(); // [9]
+
+						angleDelta = 0.0;
+						break;
+					}
+				}
+			}
+			else
+			{
+				while (--numSamples >= 0) // [6]
+				{
+					auto currentSample = (float)(std::sin(currentAngle) * level);
+
+					for (auto i = outputBuffer.getNumChannels(); --i >= 0;)
+						outputBuffer.addSample(i, startSample, currentSample);
+
+					currentAngle += angleDelta;
+					++startSample;
+				}
+			}
+		}
+	}
+
+private:
+	double currentAngle = 0.0, angleDelta = 0.0, level = 0.0, tailOff = 0.0;
+};
